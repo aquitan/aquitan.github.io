cc191e5d80a8da8a8c44986188d4c84eeef2ecb9
Sergey Sukharevskiy <30325902+aquitan@users.noreply.github.com>
2018-06-26 17:58:16 +0300

    Add files via upload

---
+++ beton/js/main.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+	$('.beton__first').click(function() {
+		alert('Click');
+	})
+
+	$('.moreAbout').click(function() {
+		$('.about__wrap').addClass("fadeIn__active");
+		$('.moreAbout').addClass("moreAbout__hide");
+		$('.lessAbout').addClass("lessAbout__show");
+		$('.about__wrap').removeClass("about__wrap");
+	})
+
+	$('.lessAbout').click(function() {
+		$('.about__wrap').removeClass("fadeIn__active");
+		$('.moreAbout').removeClass("moreAbout__hide");
+		$('.lessAbout').removeClass("lessAbout__show");
+		$('.fadeIn__active').addClass("about__wrap");
+	})
+
+	$('.menu_burger').click(function(){
+		$('.menu_burger').toggleClass('menu_burger_active');
+		$('.menu__small').toggleClass('menu__small_active');
+	})
+
+	$('.small__list_close').click(function(){
+		$('.menu_burger_active').removeClass('menu_burger_active');
+		$('.menu__small').removeClass('menu__small_active');
+	})
+})
+
+
