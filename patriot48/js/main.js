9fd05e61bb5284f18fd901bf3edb36ac1644910d
Sergey Sukharevskiy <30325902+aquitan@users.noreply.github.com>
2018-07-25 20:26:26 +0300

    Add files via upload

---
+++ patriot48/js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+	$('.drop-menu').on('click', function() {
+		$('.drop-menu').toggleClass('drop-menu-active');
+		$('.drop-down-menu').toggle('fadeIn(500)');
+	})	
+})
+
